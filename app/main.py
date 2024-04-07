931a646a8519c39df37019159630cbc833d57868
epivoca <artfom02@gmail.com>
2024-04-07 22:22:04 +0300

    Stick code together in main def

--- app/main.py
+++ app/main.py
@@ -1,50 +1,41 @@
-from typing import List
+import json
+from typing import List, Any
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel, Field
 from motor.motor_asyncio import AsyncIOMotorClient
 from bson import ObjectId
 import uvicorn
 
-
-class Characteristic(BaseModel):
-    name: str
-    value: str
-    unit: str
-
-
-class ProductCreate(BaseModel):
-    image: str
-    product_type: str
-    name: str
-    model: str
-    manufacturer: str
-    measurement_unit: str
-    gost_classification: str
-    country_of_origin: str
-    characteristics: List[Characteristic]
-
-
-class ProductDB(ProductCreate):
-    id: str = Field(None, alias="_id")
-
-    @classmethod
-    def from_mongo(cls, data: dict):
-        data["id"] = str(data["_id"])
-        del data["_id"]
-        return cls(**data)
-
-    class Config:
-        allow_population_by_field_name = True
-        arbitrary_types_allowed = True
-        json_encoders = {
-            ObjectId: str  # для конвертации ObjectId в строку при сериализации JSON
-        }
-
+from data_processors.predict_categories import Database, EFB
+from data_processors.product_name_ngram import ProductNameNgram
+from data_processors.autocompleter import NameAutoCompleter
+from fastapi.middleware.cors import CORSMiddleware
+from models import ProductDB, ProductCreate, ProductNamePredictions, ProductInfo, Category
+from parsers.wildberris import WildberriesParser
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+super_parser = WildberriesParser()
+
 client = AsyncIOMotorClient("mongodb://localhost:27017")
 db = client["product_database"]
+
+cte_storage = Database()
+
+name_auto_completer = NameAutoCompleter("./resources/sorted_words.txt")
+product_name_ngram = ProductNameNgram(tokens_path="./resources/tokens.txt")
+
+categories = EFB(path_to_np="./resources/file_numpy.npy")
+
+with open('./resources/cat2topprop.json') as json_file:
+    characteristics_storage = json.load(json_file)
 
 
 @app.post("/products/", response_model=ProductDB)
@@ -86,6 +77,63 @@ async def delete_product(product_id: str):
         return {"message": "Product deleted"}
     raise HTTPException(status_code=404, detail="Product not found")
 
+
+@app.get("/predict_names/{product_name_piece}", response_model=ProductNamePredictions)
+async def predict_product_name(product_name_piece: str):
+    print(product_name_piece)
+    if product_name_piece.endswith(' '):
+        predictions = product_name_ngram.interactive_text_generation(product_name_piece)
+        return ProductNamePredictions(names=predictions)
+
+    if len(product_name_piece.split()) < 2:
+        predictions = await name_auto_completer.get_candidates_from_t9(last_user_word=product_name_piece)
+
+        if predictions is None:
+            return ProductNamePredictions(names=[])
+
+        return ProductNamePredictions(names=predictions)
+
+    last_user_word = product_name_piece.split()[-1]
+
+    predictions = await name_auto_completer.get_candidates_from_t9(last_user_word=last_user_word)
+    names = [' '.join(product_name_piece.split()[:-1]) + ' ' + prediction for prediction in predictions]
+
+    return ProductNamePredictions(names=names)
+
+
+@app.get("/predict_categories/{product_name}", response_model=ProductInfo)
+async def predict_category(product_name: str):
+    indexes = categories.give_me_name(st=product_name, k=5)
+
+    final_categories = []
+    final_characteristics = []
+
+    parsed_info = super_parser.get_product(name=product_name)
+    for ind in indexes:
+        res = cte_storage.get_product_by_id(product_id=int(ind))
+
+        if parsed_info['features'] is not None:
+            outp = [{"key": key, "value": value} for key, value in parsed_info['features'].items()]
+            predicted_category = Category(
+                model=res.model,
+                category_name=res.name_category,
+                manufacturer=res.manufacturer,
+                characteristics=outp
+            )
+        else:
+            res_dct = {}
+            for str in characteristics_storage[res.name_category]:
+                res_dct[str] = ''
+
+            predicted_category = Category(
+                model=res.model,
+                category_name=res.name_category,
+                manufacturer=res.manufacturer,
+                characteristics=[res_dct]
+            )
+        final_categories.append(predicted_category)
+
+    return ProductInfo(categories=final_categories, characteristics=final_characteristics)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
